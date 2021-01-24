a2ce5b9f941219f0b2fde4a49c5e39145935f488
kwkt <44708388+kwQt@users.noreply.github.com>
2021-01-24 19:42:38 +0900

    List programming languages

--- main.go
+++ main.go
@@ -1,4 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sort"
+)
+
 func main() {
+	url := "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	data := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	var list []string
+	for k, v := range data {
+		t := v.(map[interface{}]interface{})["type"]
+		if t != "programming" {
+			continue
+		}
+
+		list = append(list, k.(string))
+	}
+
+	sort.Strings(list)
+
+	for _, name := range list {
+		fmt.Println(name)
+	}
 }
